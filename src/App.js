6fb10b4404b215b90b9fbec86ab084e40ba59666
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2024-09-24 00:22:20 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonContainer } from './containers/PokemonContainer';
 function App() {
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://graphql-pokemon2.vercel.app/',
+    }),
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
   );
 }
 
